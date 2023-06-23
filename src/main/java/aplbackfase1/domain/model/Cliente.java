94ae0f2976653bc6ce4c095428d80c449924282e
gabriel <gabrielalmeidads@gmail.com>
2023-06-22 23:17:50 -0300

    Alterando classe de modelo de Cliente:  - Alterando tipagem para objetos de valores  - Removendo dependências de framework

--- src/main/java/aplbackfase1/domain/model/Cliente.java
+++ src/main/java/aplbackfase1/domain/model/Cliente.java
@@ -1,38 +1,87 @@
 package aplbackfase1.domain.model;
 
-import org.hibernate.annotations.Type;
+import aplbackfase1.domain.model.valueObject.Cpf;
+import aplbackfase1.domain.model.valueObject.Email;
+import aplbackfase1.domain.model.valueObject.Nome;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import java.util.Objects;
 import java.util.UUID;
 
-@Entity
 public class Cliente {
-    @Id
-    @GeneratedValue
-    @Type(type="uuid-char")
-    private UUID idCliente;
-    private String nome;
-    private String cpf;
-    private String email;
+
+    private UUID id;
+    private Nome nome;
+    private Cpf cpf;
+    private Email email;
+
+    Cliente() {}
+
+    public Cliente(UUID id, String nome, String email, String cpf) {
+        this(id, new Nome(nome), new Email(email), new Cpf(cpf));
+    }
+
+    public Cliente(UUID id, Nome nome, Email email, Cpf cpf) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.cpf = cpf;
+    }
 
     public Cliente getCliente() {
         return this;
     }
 
-    public void identificaViaCpf(String cpf) {
+    public UUID getId() {
+        return this.id;
+    }
+
+    public Nome getNome() {
+        return this.nome;
+    }
+
+    public Email getEmail() {
+        return this.email;
+    }
+
+    public Cpf getCpf() {
+        return this.cpf;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Cliente other = (Cliente) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    public void identificaViaCpf(Cpf cpf) {
         this.cpf = cpf;
     }
 
-    public void cadastra(String nome, String email) {
+    public void cadastra(Nome nome, Email email) {
         this.nome = nome;
         this.email = email;
     }
 
     public boolean validaIdentificacao() {
-        return (!Objects.isNull(this.cpf)) || (!Objects.isNull(this.nome)) || (!Objects.isNull(this.email));
+        return this.cpf.isInvalid() || this.nome.isInvalid() || this.email.isInvalid();
     }
 
 }
