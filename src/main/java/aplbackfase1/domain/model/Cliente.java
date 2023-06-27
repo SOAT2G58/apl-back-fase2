8f6b0107177cc2b269cec90510510d51f55637f5
gabriel <gabrielalmeidads@gmail.com>
2023-06-27 01:33:56 -0300

    WIP CLIENTE:  - Ajustando adequações para arquitetura em modelo de domínio Cliente

--- src/main/java/aplbackfase1/domain/model/Cliente.java
+++ src/main/java/aplbackfase1/domain/model/Cliente.java
@@ -3,19 +3,21 @@ package aplbackfase1.domain.model;
 import aplbackfase1.domain.model.valueObject.Cpf;
 import aplbackfase1.domain.model.valueObject.Email;
 import aplbackfase1.domain.model.valueObject.Nome;
-import lombok.EqualsAndHashCode;
+import lombok.*;
 
 import java.util.UUID;
 
 @EqualsAndHashCode
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Cliente {
 
     private UUID id;
     private Nome nome;
     private Cpf cpf;
     private Email email;
-
-    Cliente() {}
 
     public Cliente(UUID id, String nome, String email, String cpf) {
         this(id, new Nome(nome), new Email(email), new Cpf(cpf));
@@ -46,19 +48,6 @@ public class Cliente {
 
     public Cpf getCpf() {
         return this.cpf;
-    }
-
-    public void identificaViaCpf(Cpf cpf) {
-        this.cpf = cpf;
-    }
-
-    public void cadastra(Nome nome, Email email) {
-        this.nome = nome;
-        this.email = email;
-    }
-
-    public boolean validaIdentificacao() {
-        return this.cpf.isInvalid() || this.nome.isInvalid() || this.email.isInvalid();
     }
 
 }
